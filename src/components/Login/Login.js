11eea063285ee45fbe0810733152dd54b4bf5f93
rirefat <rirefat.official@gmail.com>
2023-02-14 22:22:54 +0600

    done login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Login.css'
+import logo from '../../assets/images/logo2.png'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='login-area'>
+            <div className="logo-area">
+                <img src={logo} alt="" />
+            </div>
+            <form action="" className='form-area'>
+                <input type="email" name="" id="" placeholder='Your email' />
+                <input type="password" name="" id="" placeholder='Your Password' />
+                <input type="button" value="LOGIN" className='btn' />                
+            </form>
+            <span>New Here? <Link to='/sign-up'>Register Now</Link></span>
         </div>
     );
 };
