6bd008455901fbd71fb5122f4cce7f174c3469ec
rirefat <rirefat.official@gmail.com>
2023-02-14 16:08:52 +0600

    odify single product card

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Product.css';
+import { MdAddShoppingCart } from 'react-icons/md';
 
 const Product = (props) => {
     const {product_name,img,price,short_description}=props.product;
     return (
         <div className='single-product'>
+            <span className="add-to-cart"><MdAddShoppingCart></MdAddShoppingCart></span>
             <img src={img} alt="Product Image" />
-            <h3>{product_name}</h3>
+            <h3 className='title'>{product_name}</h3>
             <p>{short_description}</p>
-            <h3>${price}</h3>
+            <h3 className='price'><span>$</span>{price}</h3>
         </div>
     );
 };
