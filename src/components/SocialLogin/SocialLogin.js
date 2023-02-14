af75be4158a8619e7c0a695b2e136fb659af6060
rirefat <rirefat.official@gmail.com>
2023-02-14 22:40:25 +0600

    set signup page background img

---
+++ src/components/SocialLogin/SocialLogin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SocialLogin.css';
+
+const SocialLogin = () => {
+    return (
+        <div className='social-login'>
+            
+        </div>
+    );
+};
+
+export default SocialLogin;
