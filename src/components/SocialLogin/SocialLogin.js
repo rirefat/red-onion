84e53187eb8dda005c99d9ed06c0f31e12258f44
rirefat <rirefat.official@gmail.com>
2023-02-16 03:06:40 +0600

    done upto signin with google popup and solve env file problems

--- src/components/SocialLogin/SocialLogin.js
+++ src/components/SocialLogin/SocialLogin.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SocialLogin.css';
-
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { FcGoogle } from 'react-icons/fc';
 import { BsTwitter } from 'react-icons/bs';
 import { FaFacebookSquare } from 'react-icons/fa';
+import app from '../../firebase.init';
 
 const SocialLogin = () => {
+    const [user, setUser] = useState({});
+
+    const auth = getAuth(app);
+
+    const googleSignIN = () => {
+        const googleProvider = new GoogleAuthProvider();
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+                setUser(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+            });
+    }
+
     return (
         <div className='social-login'>
             <div className="top-section">
@@ -14,7 +36,7 @@ const SocialLogin = () => {
                 <hr />
             </div>
             <div className="socials">
-                <button className='google'><FcGoogle></FcGoogle></button>
+                <button className='google' onClick={googleSignIN}><FcGoogle></FcGoogle></button>
                 <button className='facebook'><FaFacebookSquare></FaFacebookSquare></button>
                 <button className='twitter'><BsTwitter></BsTwitter></button>
             </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,13 +1,21 @@
 import { initializeApp } from "firebase/app";
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  apiKey : process.env.REACT_APP_apiKey,
+  authDomain : process.env.REACT_APP_authDomain,
+  projectId : process.env.REACT_APP_projectId,
+  storageBucket : process.env.REACT_APP_storageBucket,
+  messagingSenderId : process.env.REACT_APP_messagingSenderId,
+  appId : process.env.REACT_APP_appId
+  
+//   apiKey: "AIzaSyC8MMaj75tWqEP1H4q3Nc-pgL7Ax4GGMdY",
+//   authDomain: "red-onion-foods-web.firebaseapp.com",
+//   projectId: "red-onion-foods-web",
+//   storageBucket: "red-onion-foods-web.appspot.com",
+//   messagingSenderId: "364856475386",
+//   appId: "1:364856475386:web:74e597cf76c6a23a9c5343"
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
